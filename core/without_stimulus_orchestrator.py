40f7a38fb7a2991f71a06f96bb30e9c471b1d3c9
cgmaiorano <celiagrace13@gmail.com>
2025-04-21 11:49:38 -0400

    implement lsl streaming for without stim

--- core/without_stimulus_orchestrator.py
+++ core/without_stimulus_orchestrator.py
@@ -1,4 +1,8 @@
+import time
 import pyzed.sl as sl
+
+from datetime import datetime
+from pylsl import StreamInfo, StreamOutlet
 
 from core import zed_parameters
 from core import export
@@ -7,21 +11,31 @@ from without_stimulus import processing
 
 
 def run(participant_ID, sequence):
-
     print(f"Beginning CAMI Protocol Sequence {sequence}")
 
     zed = sl.Camera()
 
-    # Create camera object, initialize camera parameters, start recording svo file
+    # Create camera object, initialize camera parameters
     zed_parameters.initialize_zed_parameters(zed)
+    start_time = datetime.now()
+
+    # Create new stream info for lsl, stream camera_open, change source_id from "zed2i-harlem" to appropriate device, ex: "zed2i-midtown"
+    info = StreamInfo("MotionTracking", "Markers", 1, 0, "string", "zed2i-harlem")
+    outlet = StreamOutlet(info)
+    outlet.push_sample([f"camera_open: {start_time}"])
+    time.sleep(0.1)
+
+    # start recording svo file
     export.record_svo(participant_ID, sequence, zed)
 
     # main processing
-    ordered_df = processing.body_tracking(zed)
+    ordered_df = processing.body_tracking(zed, outlet)
 
     if ordered_df is None:
-        print(f"Manual Quit... ZED Body tracking for Participant: {participant_ID} Sequence: {sequence} INCOMPLETED.")
-        return 
-    
+        print(
+            f"Manual Quit... ZED Body tracking for Participant: {participant_ID} Sequence: {sequence} INCOMPLETED."
+        )
+        return
+
     # save data
     export.save_sequence(participant_ID, sequence, ordered_df)
