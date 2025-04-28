9e73e50edaa33772348ec211a7cf565dd9078d99
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2025-04-28 12:07:42 -0400

    Update without_stimulus_orchestrator.py
    move start time

--- core/without_stimulus_orchestrator.py
+++ core/without_stimulus_orchestrator.py
@@ -17,7 +17,6 @@ def run(participant_ID, sequence):
 
     # Create camera object, initialize camera parameters
     zed_parameters.initialize_zed_parameters(zed)
-    start_time = datetime.now()
 
     # Create new stream info for lsl, stream camera_open, change source_id from "zed2i-harlem" to appropriate device, ex: "zed2i-midtown"
     info = StreamInfo("MotionTracking", "Markers", 1, 0, "string", "zed2i-harlem")
@@ -31,6 +30,7 @@ def run(participant_ID, sequence):
     # start recording svo file
     export.record_svo(participant_ID, sequence, zed)
     
+    start_time = datetime.now()
     outlet.push_sample([f"camera_open: {start_time}"])
     
     # main processing
